48042dd3d15edfec01ad3e756097106fc430a0c2
Masashi Fujita <objectxtreme@gmail.com>
2014-10-11 13:27:09 +0900

    Create a test project

---
+++ src/Lib.hs
@@ -0,0 +1,5 @@
+module Lib (exported) where
+
+exported :: IO ()
+exported = do
+    putStrLn "Hello from exported"
---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+module Main (main) where
+
+import Lib
+
+-- | The program entry.
+main :: IO ()
+main = do
+    putStrLn "Hello World"
+    exported
+
+--- END OF FILE ---
